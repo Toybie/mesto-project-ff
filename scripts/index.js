c01ac9e93aa4fa3715f9882f6dbac558b5c2403b
Toybie <50700292+Toybie@users.noreply.github.com>
2024-09-06 14:20:30 +0300

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCardElement(data, onDelete) {
+    const cardElement = document.getElementById('card-template').cloneNode(true);
+    cardElement.querySelector('.title').textContent = data.title;
+    cardElement.querySelector('.description').textContent = data.description;
+    cardElement.querySelector('.delete-icon').addEventListener('click', function() {
+      onDelete();
+    });
+    return cardElement;
+  }
+  
+  function renderCards(cards) {
+    const placeList = document.querySelector('.places__list');
+    cards.forEach(function(cardData) {
+      const cardElement = createCardElement(cardData, function() {
+        console.log('Карточка была удалена');
+      });
+      placeList.appendChild(cardElement);
+    });
+  }
+  
+  const initialCards = [
+    { title: 'Место 1', description: 'Описание места 1' },
+    { title: 'Место 2', description: 'Описание места 2' },
+    { title: 'Место 3', description: 'Описание места 3' },
+    { title: 'Место 4', description: 'Описание места 4' },
+    { title: 'Место 5', description: 'Описание места 5' },
+    { title: 'Место 6', description: 'Описание места 6' },
+  ];
+  
+  renderCards(initialCards);
