2567c76a4dcd6a7ca2623ed9fca5cbfa892d9951
Boris Tauber <tauber.ba@gmail.com>
2024-09-06 19:42:39 +0300

    Готовность к сдаче

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,22 @@
-function removeCard(cardElement) {
-  cardElement.parentNode.removeChild(cardElement);
-}
-
 function createCardElement(data, deleteCard) {
   const cardElement = document.querySelector('#card-template').content.cloneNode(true);
-  cardElement.querySelector('.card__image').src = data.link;
-  cardElement.querySelector('.card__title').textContent = data.title;
-  cardElement.querySelector('.card__description').textContent = data.name;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));
-  return cardElement;
+  const card = cardElement.querySelector('.card'); 
+  card.querySelector('.card__image').src = data.link;
+  card.querySelector('.card__title').textContent = data.title;
+  card.querySelector('.card__description').textContent = data.name;
+  card.querySelector('.card__delete-button').addEventListener('click', () => removeCard(card));
+  return card;
+}
+
+function removeCard(cardElement) {
+  cardElement.remove();
 }
 
 function renderCards(cards) {
   const placeList = document.querySelector('.places__list');
   cards.forEach(function(cardData) {
     const cardElement = createCardElement(cardData, removeCard); 
-    placeList.appendChild(cardElement); 
+    placeList.appendChild(cardElement);
   });
 }
 
