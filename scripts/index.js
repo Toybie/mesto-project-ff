e2dd0539734fbce46e4b47c2c671963a4184c32a
Toybie <50700292+Toybie@users.noreply.github.com>
2024-09-06 17:51:44 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,22 @@
-function createCardElement(data, deleteCard) { 
-  const cardElement = document.querySelector('#card-template').content.cloneNode(true); 
-  cardElement.querySelector('.card__image').src = data.link; 
-  cardElement.querySelector('.card__title').textContent = data.title; 
-  cardElement.querySelector('.card__description').textContent = data.name; 
-  cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));  
-  return cardElement;  
-} 
- 
-function removeCard(cardElement) { 
-  cardElement.remove(); 
-} 
- 
-function renderCards(cards) { 
-  const placeList = document.querySelector('.places__list'); 
-  cards.forEach(function(cardData) { 
-    const cardElement = createCardElement(cardData); 
+function removeCard(cardElement) {
+  cardElement.parentNode.removeChild(cardElement);
+}
+
+function createCardElement(data, deleteCard) {
+  const cardElement = document.querySelector('#card-template').content.cloneNode(true);
+  cardElement.querySelector('.card__image').src = data.link;
+  cardElement.querySelector('.card__title').textContent = data.title;
+  cardElement.querySelector('.card__description').textContent = data.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));
+  return cardElement;
+}
+
+function renderCards(cards) {
+  const placeList = document.querySelector('.places__list');
+  cards.forEach(function(cardData) {
+    const cardElement = createCardElement(cardData, removeCard); 
     placeList.appendChild(cardElement); 
-  }); 
-} 
- 
+  });
+}
+
 renderCards(initialCards);
