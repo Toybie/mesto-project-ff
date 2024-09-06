d4a10377a330830cc06ae43f6efc306feefc8684
Toybie <50700292+Toybie@users.noreply.github.com>
2024-09-06 17:22:05 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,22 @@
-function createCardElement(data, onDelete) {
-  const cardElement = document.querySelector('#card-template').content.cloneNode(true);
-  cardElement.querySelector('.card__image').src = data.link;
-  cardElement.querySelector('.card__title').textContent = data.title;
-  cardElement.querySelector('.card__description').textContent = data.name;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', onDelete); 
-  return cardElement; 
-}
-
-/* Функция удаления */
-const card = document.getElementsByName('.card');
-const deleteButton = card.querySelector('.card__delete-button');
-deleteButton.addEventListener('click', function () {
-  const listItem = deleteButton.closest('.card__delete-button');
-  listItem.remove();
-});
-
-function renderCards(cards) {
-  const placeList = document.querySelector('.places__list');
-  cards.forEach(function(cardData) {
-    const cardElement = createCardElement(cardData);
-    placeList.appendChild(cardElement);
-  });
-}
-
+function createCardElement(data, deleteCard) { 
+  const cardElement = document.querySelector('#card-template').content.cloneNode(true); 
+  cardElement.querySelector('.card__image').src = data.link; 
+  cardElement.querySelector('.card__title').textContent = data.title; 
+  cardElement.querySelector('.card__description').textContent = data.name; 
+  cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));  
+  return cardElement;  
+} 
+ 
+function removeCard(cardElement) { 
+  cardElement.remove(); 
+} 
+ 
+function renderCards(cards) { 
+  const placeList = document.querySelector('.places__list'); 
+  cards.forEach(function(cardData) { 
+    const cardElement = createCardElement(cardData); 
+    placeList.appendChild(cardElement); 
+  }); 
+} 
+ 
 renderCards(initialCards);
