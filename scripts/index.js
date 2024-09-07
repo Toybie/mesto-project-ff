ee321a3a9c0a8eb2b82a7e3f6c0e0016d8d8b50b
Boris Tauber <tauber.ba@gmail.com>
2024-09-07 08:49:34 +0300

    Правки ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,8 @@ function createCardElement(data, deleteCard) {
   const cardElement = document.querySelector('#card-template').content.cloneNode(true);
   const card = cardElement.querySelector('.card'); 
   card.querySelector('.card__image').src = data.link;
-  card.querySelector('.card__title').textContent = data.title;
-  card.querySelector('.card__description').textContent = data.name;
-  card.querySelector('.card__delete-button').addEventListener('click', () => removeCard(card));
+  card.querySelector('.card__title').textContent = data.name;
+  card.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(card));
   return card;
 }
 
